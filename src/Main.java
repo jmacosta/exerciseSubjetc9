210a09d8cc6c119bfd1395379d8923c4118a384d
Juan Manuel Acosta <jmab2k@gmail.com>
2022-05-31 21:36:00 +0200

    Result execise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cliente cliente = new Cliente ();
+        cliente.edad = 42;
+        cliente.nombre = "Juanma";
+        cliente.telefono = "954372548";
+        cliente.credito = 2500.50f;
+        System.out.println("El Cliente "+ cliente.nombre + " tiene "+ cliente.edad + " años y su teléfono es "
+                +cliente.telefono + " tiene un crédito máximo de "+ cliente.credito + "€" );
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad = 40;
+        trabajador.nombre = "Manuel";
+        trabajador.telefono = "954383600";
+        trabajador.salario = 1000.00f;
+        System.out.println("El Trabajador "+ trabajador.nombre + " tiene "+ trabajador.edad + " años y su teléfono es "
+                +trabajador.telefono + " tiene un salario de "+ trabajador.salario + "€" );
+
     }
+}
+class Persona{
+    public int edad;
+    public String nombre;
+    public String telefono;
+
+}
+class Cliente extends Persona{
+    public float credito;
+}
+class Trabajador extends Persona{
+    public float salario;
 }
